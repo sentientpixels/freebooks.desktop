112094fb24561260960c82704e00b998f275284a
Abdullah Alam <abdullahalam@protonmail.com>
2025-03-10 12:43:55 -0400

    setup mantine and fontawesome

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,30 @@
 import {useState} from 'react';
-import logo from './assets/images/logo-universal.png';
-import './App.css';
 import {Greet} from "../wailsjs/go/main/App";
 
-function App() {
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
-    const [name, setName] = useState('');
-    const updateName = (e: any) => setName(e.target.value);
-    const updateResultText = (result: string) => setResultText(result);
+import { useMantineTheme } from '@mantine/core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGear } from '@fortawesome/free-solid-svg-icons'
 
-    function greet() {
-        Greet(name).then(updateResultText);
-    }
+import './styles/App.scss'
+import whitelogo from './assets/white_logo.svg'
+
+
+
+
+function App() {
+
+    const theme = useMantineTheme();
+
+    // function greet() {
+    //     Greet(name).then(updateResultText);
+    // }
 
     return (
-        <div id="App">
-            <img src={logo} id="logo" alt="logo"/>
-            <div id="result" className="result">{resultText}</div>
-            <div id="input" className="input-box">
-                <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
-                <button className="btn" onClick={greet}>Greet</button>
+        <div id="App" className="App">
+            <div className="header">
+                <img src={whitelogo} className='logo' />
+                <h1 className='title'>FreeBooks</h1>
+                <FontAwesomeIcon icon={faGear} className='settings' />
             </div>
         </div>
     )
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,34 @@
-import React from 'react'
 import {createRoot} from 'react-dom/client'
-import './style.css'
 import App from './App'
+import '@mantine/core/styles.css';
+import { createTheme, MantineProvider, MantineColorsTuple } from '@mantine/core';
 
+// mantine
+const themeColors: MantineColorsTuple = [
+    "#e7fdef",
+    "#d6f6e1",
+    "#afebc3",
+    "#84dfa3",
+    "#61d587",
+    "#4acf76",
+    "#3ccc6c",
+    "#2cb45b",
+    "#21a04f",
+    "#0b8b41"
+  ];
+  
+const theme = createTheme({
+    colors: {
+        themeColors,
+    }
+});
+
+// react stuff
 const container = document.getElementById('root')
-
 const root = createRoot(container!)
 
 root.render(
-    <React.StrictMode>
+    <MantineProvider theme={theme}>
         <App/>
-    </React.StrictMode>
+    </MantineProvider>
 )
